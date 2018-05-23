df57da497e17d3a05558e249c9476f4acc177d89
kard <kard@student.ethz.ch>
2018-05-23 20:19:47 +0200

    Added the RdmaConnectionException class

---
+++ client_proxy/src/main/java/com/acn/rdma/client_proxy/GraphException.java
@@ -0,0 +1,60 @@
+package edu.kit.informatik;
+
+/**
+ * The RdmaConnectionException occurs when an error occured during the communication in the rdma
+ * connection between the client and the browser .
+ * 
+ * @author ardkastrati
+ * @version 1
+ */
+public class RdmaConnectionException extends Exception {
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new RdmaConnectionExcpeption class
+     */
+    public RdmaConnectionException() {
+        super();
+    }
+
+    /**
+     * Constructs a new RdmaConnectionException class with an argument indicating
+     * the exception.
+     * 
+     * @param message
+     *            The message indicating the problem.
+     */
+    public RdmaConnectionException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new RdmaConnectionException class with an argument indicating
+     * the cause of the exception.
+     * 
+     * @param cause
+     *            The message indicating the cause of the problem.
+     */
+    public RdmaConnectionException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new RdmaConnectionException class with an argument indicating
+     * the exception and the cause of the exception.
+     * 
+     * @param cause
+     *            The message indicating the cause of the problem.
+     * @param message
+     *            The message indicating the problem.           
+     */
+    public RdmaConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
